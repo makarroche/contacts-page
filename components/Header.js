1c353056598d61af1a307b7b343c52d1f1efe6d7
makarroche <macarena@mynft.com>
2023-09-16 18:47:42 -0300

    First Commit

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-blue">Contacts</h1>
+      <hr className="bg-danger border-2 border-top border-danger" />
+    </>
+  );
+};
+
+export default Header;
