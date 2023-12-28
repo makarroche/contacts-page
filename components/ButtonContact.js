76d6587d990df3267a7d09d4188bafed3998e924
makarroche <macarena@mynft.com>
2023-12-28 00:41:19 -0300

    formatting

--- components/ButtonContact.js
+++ components/ButtonContact.js
@@ -1,19 +1,22 @@
-import { useEffect } from "react";
 import { Button } from "react-bootstrap";
 
+const ButtonContact = ({ type, text, onClick, disabled, setShowToast }) => {
+  const handleOnClick = () => {
+    if (text === "Add new contact") setShowToast(false);
+    onClick(true);
+  };
 
-
-const ButtonContact = ({type, text, onClick, disabled, setShowToast}) => {
-
-    const handleOnClick = () =>  {
-        if(text === "Add new contact") setShowToast(false);
-        onClick(true);
-    }
-
-    return (
-     <Button id ="add-new-contact" className = "text-white" variant={type} onClick={handleOnClick} disabled={disabled}>{text}</Button>
-    );
-
-}
+  return (
+    <Button
+      id="add-new-contact"
+      className="text-white"
+      variant={type}
+      onClick={handleOnClick}
+      disabled={disabled}
+    >
+      {text}
+    </Button>
+  );
+};
 
 export default ButtonContact;
